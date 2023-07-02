55d366b9d1d8b4989ddfb4a4938bb920c954b970
Creidl <tannebaum.mariusz@gmail.com>
2023-07-02 23:00:44 +0200

    Home work - day 4

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string name = "John";
+bool isWomen = false;
+int age = 29;
+
+if (isWomen)
+    if (age < 30)
+        Console.WriteLine("Kobieta poniżej 30 lat.");
+    else 
+        if (name == "Ewa" && age == 33)
+            Console.WriteLine("Ewa, lat 33.");
+        else
+            Console.WriteLine(name + " jest dojrzałą kobietą."); 
+else
+    if (age < 18)
+        Console.WriteLine("Niepełnoletni meżczyzna.");
+    else
+        Console.WriteLine(name + " jest pełnoletni.");
